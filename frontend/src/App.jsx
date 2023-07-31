b44801ddc3d76acd3340b365d05218fb8fc05af5
HpJkR <m.hertereau@gmail.com>
2023-07-31 08:20:57 +0200

    Create Online Status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.scss"
 
 // import Home from "./pages/Home"
 import NavBar from "./components/NavBar/Navbar"
+import OnlineStatus from "./components/UserProfil/OnlineStatus"
 
 function App() {
   return (
     <>
       <div className="App">
+        <OnlineStatus />
         <NavBar />
         {/* <Home /> */}
       </div>
