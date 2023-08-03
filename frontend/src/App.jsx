e34bbfda7b06f0e8fd173c93241e42501eb6e02d
HpJkR <m.hertereau@gmail.com>
2023-08-03 11:37:05 +0200

    create landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.scss"
 
 // import Home from "./pages/Home"
-import NavBar from "./components/NavBar/Navbar"
-import OnlineStatus from "./components/UserProfil/OnlineStatus"
+// import NavBar from "./components/NavBar/Navbar"
+// import OnlineStatus from "./components/UserProfil/OnlineStatus"
+import LandingPage from "./pages/LandingPage"
 
 function App() {
   return (
     <>
       <div className="App">
-        <OnlineStatus />
-        <NavBar />
+        {/* <OnlineStatus />
+        <NavBar /> */}
+        <LandingPage />
         {/* <Home /> */}
       </div>
     </>
