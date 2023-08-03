d29cd1637a3695c7a3299cbd891a9be2c86477e3
Nestadar <Nestadar@gmail.com>
2023-08-03 18:00:37 +0200

    Nearly finished or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.scss"
 
 // import Home from "./pages/Home"
-import NavBar from "./components/NavBar/Navbar"
-import OnlineStatus from "./components/UserProfil/OnlineStatus"
+// import NavBar from "./components/NavBar/Navbar"
+// import OnlineStatus from "./components/UserProfil/OnlineStatus"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
   return (
     <>
       <div className="App">
-        <OnlineStatus />
-        <NavBar />
+        <Dashboard />
+        {/* <OnlineStatus /> */}
+        {/* <NavBar /> */}
         {/* <Home /> */}
       </div>
     </>
