da1c4e198f222367b7c3f5b23e08c4df1581e5d3
Dagets <inard.frederic@gmail.com>
2023-08-04 14:53:33 +0200

    ok

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const cors = require("cors")
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3001",
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
     optionsSuccessStatus: 200,
   })
 )
