02c9daa87185c83bc71b462daa873d9a32e3481e
Dagets <inard.frederic@gmail.com>
2023-08-04 16:48:58 +0200

    ok3

--- frontend/src/components/NewTopic/NewTopic.jsx
+++ frontend/src/components/NewTopic/NewTopic.jsx
@@ -1,52 +1,14 @@
-// import React, { useState } from "react"
-// import axios from "axios"
-
-// export default function NewTopic() {
-//   const [texte, setTexte] = useState("")
-
-//   const handleChange = (e) => {
-//     setTexte(e.target.value)
-//   }
-
-//   return (
-//     <div>
-//       <p>nom</p>
-//       <input
-//         type="text"
-//         value={texte}
-//         onChange={handleChange}
-//         placeholder="Saisissez du texte ici"
-//       />
-
-//       <button onClick={handleSubmit}>Créer ma partie</button>
-//     </div>
-//   )
-// }
-
 import "./NewTopic.scss"
 import axios from "axios"
 import React, { useState } from "react"
 
 export default function NewTopic() {
-  // const [newTopic, setNewTopic] = useState([])
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:4242/topics")
-  //     .then((res) => setNewTopic(res.data))
-  // }, [])
-
   const handleCreateTopic = () => {
     axios
       .post("http://localhost:4242/topics", {
         title: nom,
         categories_id: categorie,
-        // role_playing_game_id: rpgID,
-        //   gm_profiles_id: gm,
-        //   schedule: date,
-        //   location: place,
-        //   max_players_capacity: playersCapacity,
-        //   description: desc,
-        //   filters_id: filterID,
+        users_id: user,
       })
       .then((res) => {
         if (res.status === 200) {
@@ -60,6 +22,7 @@ export default function NewTopic() {
 
   const [nom, setNom] = useState("")
   const [categorie, setCategorie] = useState("")
+  const [user, setUser] = useState("")
 
   return (
     <>
@@ -72,6 +35,12 @@ export default function NewTopic() {
         type="text"
         placeholder="Catégorie du Topic"
         onChange={(e) => setCategorie(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="users_id"
+        onChange={(e) => setUser(e.target.value)}
       />
       <button type="reset" onClick={handleCreateTopic}>
         Créer mon Topic
