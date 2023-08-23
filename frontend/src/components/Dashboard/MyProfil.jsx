a3a776d722618a094f4f8d5b5ce30f17587f25ca
Nestadar <Nestadar@gmail.com>
2023-08-23 12:26:32 +0200

    button and some links added

--- frontend/src/components/Dashboard/MyProfil.jsx
+++ frontend/src/components/Dashboard/MyProfil.jsx
@@ -1,11 +1,29 @@
 import "./MyProfil.scss"
-
+// import axios from "axios"
+import React, { useState } from "react"
+// import { Link } from "react-router-dom"
 import iconProfile from "../../assets/icon-dashboard/iconProfile.png"
 import Add2 from "../../assets/icon-dashboard/Add2.png"
 import Edit from "../../assets/icon-dashboard/Edit.png"
 import Location from "../../assets/icon-dashboard/Location.png"
 
 const MyProfil = () => {
+  // const [games, setGames] = useState([])
+  // useEffect(() => {
+  //   axios.get("http://localhost:4242/games")
+  //   .then((res) => setGames(res.data))
+  //   console.info(games)
+  // }, [])
+
+  const [userPicture, setUserPicture] = useState(null)
+  const [imageUrl, setImageUrl] = useState(null)
+
+  const handlePictureChange = (e) => {
+    const picture = e.target.files[0]
+    setUserPicture(picture)
+    setImageUrl(URL.createObjectURL(picture))
+  }
+
   return (
     <div className="myProfil">
       <div className="titleProfil">
@@ -14,12 +32,27 @@ const MyProfil = () => {
       </div>
       <div className="contentMyProfil">
         <div className="editProfile">
-          <img id="logoEdit" src={Edit} alt="logo of a pen" />
-          <p>Edit Profile</p>
+          <button type="button" id="editButton">
+            <img id="logoEdit" src={Edit} alt="logo of a pen" />
+            <p>Edit Profile</p>
+          </button>
         </div>
         <div className="topProfile">
           <div className="logoAdd2">
-            <img id="logoAdd2" src={Add2} alt="logo of a cross" />
+            <label htmlFor="buttonPicture">
+              {userPicture ? (
+                <img src={imageUrl} alt="userPicture" id="logoAdd2" />
+              ) : (
+                <img id="logoAdd2" src={Add2} alt="logo of a cross" />
+              )}
+            </label>
+            <input
+              type="file"
+              id="buttonPicture"
+              accept="image/*"
+              onChange={handlePictureChange}
+              style={{ display: "none" }}
+            />
           </div>
           <div className="centralDiv">
             <div className="userInfoProfile">
@@ -36,7 +69,7 @@ const MyProfil = () => {
             </div>
           </div>
           <div className="profileButton">
-            <button type="button"> NOT DEFINED</button>
+            <p> NOT DEFINED</p>
           </div>
         </div>
         <div className="middleProfile">
