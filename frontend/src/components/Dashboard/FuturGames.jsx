cb0ca1c5ec5e4ecee38544009627eca40dd54a8e
Johanna Hilaire <johannahilairepro@gmail.com>
2023-08-29 10:36:04 +0200

    deleted preloader, need to fix 3D weight to make it work

--- frontend/src/components/Dashboard/FuturGames.jsx
+++ frontend/src/components/Dashboard/FuturGames.jsx
@@ -1,23 +1,38 @@
-// import axios from "axios"
-// import React, { useEffect, useState } from "react"
+import axios from "axios"
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import Search from "../../assets/icon-dashboard/Search.png"
 import Dice from "../../assets/icon-dashboard/Dice.png"
 import Add from "../../assets/icon-dashboard/Add.png"
 
 export default function futurGames() {
-  // const [games, setGames] = useState([])
-  // useEffect(() => {
-  //   axios.get("http://localhost:4242/games")
-  //   .then((res) => setGames(res.data))
-  //   console.info(games)
-  // }, [])
+  const [games, setGames] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:4242/games").then((res) => setGames(res.data))
+    console.info(games)
+  }, [])
 
   return (
     <div className="myFutureGames">
       <img id="logoFutureGames" src={Dice} alt="logo of D20" />
       <h2>MY FUTURE GAMES</h2>
       <div className="contentFutureGames">
+        {/* <div>
+          {games.map((game) => (
+            <div key={game.id}>
+              <ul>
+                <li>{game.role_playing_game_id}</li>
+                <li>{game.gm_profiles_id}</li>
+                <li>{game.schedule}</li>
+                <li>{game.location}</li>
+                <li>{game.max_players_capacity}</li>
+                <li>{game.description}</li>
+                <li>{game.filters_id}</li>
+              </ul>
+            </div>
+          ))}
+        </div> */}
+
         <div className="contentWindow">
           <div className="divButtonsss">
             <div className="logoContentFG">
@@ -47,20 +62,3 @@ export default function futurGames() {
     </div>
   )
 }
-
-// <div>
-//   {" "}
-//   {games.map((game) => (
-//     <div key={game.id}>
-//       <ul>
-//         <li>{game.role_playing_game_id}</li>
-//         <li>{game.gm_profiles_id}</li>
-//         <li>{game.schedule}</li>
-//         <li>{game.location}</li>
-//         <li>{game.max_players_capacity}</li>
-//         <li>{game.description}</li>
-//         <li>{game.filters_id}</li>
-//       </ul>
-//     </div>
-//   ))}
-// </div>
