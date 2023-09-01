af1bd6f455e6c80933ebffbc1ad5da43a013b526
MEHDI ZAMITTI <121120626+mehdizamm@users.noreply.github.com>
2023-09-01 10:05:37 +0200

    2ndsave

--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -1,7 +1,36 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
+import Cookies from "js-cookie"
 import "./Profil.scss"
+import axios from "axios"
+import AuthContext from "../components/AuthContext/AuthContext"
 
 const Profil = () => {
+  const { user } = useContext(AuthContext)
+  const tokenFromCookie = Cookies.get("authToken")
+  const [gameData, setGameData] = useState({})
+
+  const headers = {
+    Authorization: `Bearer ${tokenFromCookie}`,
+  }
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4242/role-playing-games", { headers })
+      .then((response) => {
+        setGameData(response.data[1])
+        console.info(gameData)
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error)
+      })
+  }, [])
+
+  const shortDate = String(user.registration_date)
+    .substring(0, 10)
+    .split("-")
+    .reverse()
+    .join("-")
+
   return (
     <div className="mainContainerProfil">
       <div className="leftBoxMain">
@@ -20,7 +49,12 @@ const Profil = () => {
           <div className="titleProfil">PUBLIC INFORMATION</div>
           <div className="topBoxProfil">
             <div className="boxPhoto">
-              balise imageUser registrationDateUser
+              <img
+                src={`${import.meta.env.VITE_BACKEND_URL}/${
+                  user.profil_picture
+                }`}
+              />
+              <span>{shortDate}</span>
             </div>
             <div className="boxName">INPUT NAME</div>
             <div className="localisationBox">INPUT/PAYS INPUT/PARIS</div>
