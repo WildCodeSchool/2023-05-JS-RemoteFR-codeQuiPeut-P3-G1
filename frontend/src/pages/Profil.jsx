d4668eb35ffa22b313f8dcf299f6cb0290431fde
Nestadar <Nestadar@gmail.com>
2023-09-05 16:52:58 +0200

    pictureupdate resolved, fixing validation form

--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -4,25 +4,102 @@ import axios from "axios"
 import AuthContext from "../components/AuthContext/AuthContext"
 import iconProfil from "../assets/Profil/iconProfil.png.png"
 import questionMark from "../assets/Profil/questionMark.png.png"
+import Add2 from "../assets/icon-dashboard/Add2.png"
 
 const Profil = () => {
-  const { user } = useContext(AuthContext)
-  const tokenFromCookie = Cookies.get("authToken")
-  const [gameData, setGameData] = useState({})
-  // const [isClicked, setIsClicked] = useState(false)
+  const { user, setUser, setUsers } = useContext(AuthContext)
+  // const [gameData, setGameData] = useState({})
   const [isEditing, setIsEditing] = useState(false)
-  const [switchPassword, setSwitchpassWord] = useState(false)
-  const [newUsername, setNewUsername] = useState(user.username)
-  const [isEditingBioBox, setIsEditingBioBox] = useState(false)
-  const [newDescription, setNewDescription] = useState(
-    user.description_as_player
-  )
+  const idUser = Cookies.get("idUser")
+  const [imageUrl, setImageUrl] = useState(null)
+  // const [newUsername, setNewUsername] = useState(user.username)
+  // const [switchPassword, setSwitchpassWord] = useState(false)
+  // const [isEditingBioBox, setIsEditingBioBox] = useState(false)
+  // const [newDescription, setNewDescription] = useState(
+  //   user.description_as_player
+  // )
   const [buttonStates, setButtonStates] = useState({
     profil: false,
     myGames: false,
     social: false,
   })
 
+  const tokenFromCookie = Cookies.get("authToken")
+  const headers = {
+    Authorization: `Bearer ${tokenFromCookie}`,
+  }
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4242/users`, { headers })
+      .then((res) => {
+        setUsers(res.data)
+      })
+      .catch((err) => {
+        console.error("Problème lors du chargement des users", err)
+      })
+  }, [])
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4242/users/${idUser}`, { headers })
+      .then((res) => {
+        setUser(res.data)
+      })
+      .catch((err) => {
+        console.error("Problème lors du chargement des users", err)
+      })
+  }, [])
+
+  useEffect(() => {
+    setImageUrl(`${import.meta.env.VITE_BACKEND_URL}/${user.profil_picture}`)
+  }, [user.profil_picture])
+
+  const updateProfilPictureOnServer = async (userId, formData) => {
+    try {
+      const response = await axios.put(
+        `http://localhost:4242/users/${userId}/upload`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data", // Important for sending files,
+            Authorization: `Bearer ${tokenFromCookie}`,
+          },
+        }
+      )
+      return response.data
+    } catch (error) {
+      console.error(
+        "Erreur lors de la mise à jour de la photo de profil :",
+        error
+      )
+      throw error
+    }
+  }
+
+  const handlePictureChange = (e) => {
+    const picture = e.target.files[0]
+
+    // Créez un objet FormData pour envoyer la photo
+    const formData = new FormData()
+    formData.append("myFile", picture)
+
+    setImageUrl(URL.createObjectURL(picture))
+
+    // Appel de la fonction pour mettre à jour la photo de profil sur le serveur
+    updateProfilPictureOnServer(user.id, formData)
+  }
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:4242/role-playing-games", { headers })
+  //     .then((response) => {
+  //       setGameData(response.data[1])
+  //       console.info(gameData)
+  //     })
+  //     .catch((error) => {
+  //       console.error("An error occurred:", error)
+  //     })
+  // }, [])
   // const handleKeyPressEnter = (event) => {
   //   if (event.key === "Enter") {
   //     buttonRef.current.click()
@@ -42,22 +119,6 @@ const Profil = () => {
   //   };
   // }, []);
   // onKeyDown={handleKeyPressEnter} // mettre au niveau des boutons fermants
-
-  const headers = {
-    Authorization: `Bearer ${tokenFromCookie}`,
-  }
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:4242/role-playing-games", { headers })
-      .then((response) => {
-        setGameData(response.data[1])
-        console.info(gameData)
-      })
-      .catch((error) => {
-        console.error("An error occurred:", error)
-      })
-  }, [])
 
   const shortDate = String(user.registration_date)
     .substring(0, 10)
@@ -116,136 +177,104 @@ const Profil = () => {
           </div>
         </div>
       </div>
-
-      <div className="rightBoxMain">
-        <div className="mainTitleProfil">
-          <img src={iconProfil} />
-          <h1>PROFILE</h1>
-        </div>
-
-        <div className="bigBoxRight">
-          <div className="titleProfil">PUBLIC INFORMATIONS</div>
-          <div className="topBoxProfil">
-            <div className="boxDateAndBoxPhoto">
-              <div className="boxPhoto">
-                <img
-                  src={`${import.meta.env.VITE_BACKEND_URL}/${
-                    user.profil_picture
-                  }`}
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditing(!isEditing)
+        }}
+      >
+        Click tamere
+      </button>
+      {isEditing === true ? (
+        <div className="rightBoxMain">
+          <div className="mainTitleProfil">
+            <img src={iconProfil} />
+            <h1>PROFILE</h1>
+          </div>
+          <div className="bigBoxRight">
+            <div className="titleProfil">PUBLIC INFORMATIONS</div>
+            <div className="topBoxProfil">
+              <div className="boxDateAndBoxPhoto">
+                <div className="boxPhoto">
+                  <label htmlFor="buttonPicture">
+                    {user.profil_picture !== null ? (
+                      <img
+                        src={imageUrl}
+                        alt="userPicture"
+                        name="myFile"
+                        className="userPicture"
+                        id="profilPictureForm"
+                      />
+                    ) : (
+                      <img
+                        className="userPicture"
+                        src={Add2}
+                        alt="logo of a cross"
+                      />
+                    )}
+                  </label>
+                  <input
+                    type="file"
+                    id="buttonPicture"
+                    accept="image/*"
+                    onChange={(e) => {
+                      handlePictureChange(e)
+                    }}
+                    style={{ display: "none" }}
+                  />
+                </div>
+                <span>Register since {shortDate}</span>
+              </div>
+              <div className="boxName"></div>
+              <div className="localisationBox">
+                <span>Country</span>
+                <input
+                  type="text"
+                  className="inputCountryCity"
+                  placeholder="Enter your country"
+                />
+                <span>City</span>
+                <input
+                  type="text"
+                  className="inputCountryCity"
+                  placeholder="Enter Your City"
                 />
               </div>
-              <span>Register since {shortDate}</span>
             </div>
-            <div className="boxName">
-              {isEditing ? (
-                <div className="editNameDiv">
-                  <input
-                    className="inputNewName"
-                    type="text"
-                    value={newUsername}
-                    onChange={(e) => setNewUsername(e.target.value)}
-                  />
-                  <button
-                    id="buttonNewName"
-                    onClick={() => setIsEditing(false)}
-                    // onKeyDown={handleKeyPressEnter}
-                  >
-                    Save
-                  </button>
-                </div>
-              ) : (
-                <div className="userNameDiv">
-                  UserName
-                  <button
-                    id="userNameButton"
-                    onClick={() => setIsEditing(true)}
-                  >
-                    {user.username}
-                  </button>
-                </div>
-              )}
+            <div className="bioBoxProfil">
+              <div className="bioBoxProfilText">
+                <div className="bioBoxProfilTitle">Bio on Profil</div>
+              </div>
             </div>
-            <div className="localisationBox">
-              <span>Country</span>
-              <input
-                type="text"
-                className="inputCountryCity"
-                placeholder="Enter your country"
-              />
-              <span>City</span>
-              <input
-                type="text"
-                className="inputCountryCity"
-                placeholder="Enter Your City"
-              />
-            </div>
-          </div>
-          <div className="bioBoxProfil">
-            <div className="bioBoxProfilText">
-              <div className="bioBoxProfilTitle">Bio on Profil</div>
-              {isEditingBioBox ? (
-                <div className="editDescriptionBox">
-                  <input
-                    className="inputNewDescription"
-                    type="text"
-                    value={newDescription}
-                    onChange={(e) => setNewDescription(e.target.value)}
-                  />
-                  <button
-                    type="button"
-                    id="buttonNewDescription"
-                    onClick={() => setIsEditingBioBox(false)}
-                    // onKeyDown={handleKeyPressEnter}
-                  >
-                    Save
-                  </button>
-                </div>
-              ) : (
-                <div className="userDescriptionDiv">
-                  <button
-                    id="userNameDescription"
-                    onClick={() => setIsEditingBioBox(true)}
-                  >
-                    {user.description_as_player}
-                  </button>
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="gameBoxProfil">
-            <div className="gameBoxProfilTitle">
-              <h2>SEARCH TO PLAY ON</h2>
+            <div className="gameBoxProfil">
+              <div className="gameBoxProfilTitle">
+                <h2>SEARCH TO PLAY ON</h2>
+              </div>
+              <div className="hrDiv">
+                <hr />
+              </div>
+
+              <div className="gameBoxGamesList">composant gamelist</div>
             </div>
             <div className="hrDiv">
               <hr />
             </div>
+            <div className="bottomBoxProfil">
+              <div className="privateInfoBox">
+                <h3>PERSONAL</h3>
+                <h3>INFORMATIONS</h3>
+              </div>
 
-            <div className="gameBoxGamesList">composant gamelist</div>
-          </div>
-          <div className="hrDiv">
-            <hr />
-          </div>
-          <div className="bottomBoxProfil">
-            <div className="privateInfoBox">
-              <h3>PERSONAL</h3>
-              <h3>INFORMATIONS</h3>
-            </div>
-
-            <hr />
-            <div className="mailBox">
-              Email adress
-              <input
-                type="email"
-                className="inputBottomProfil"
-                placeholder="Enter Your Email"
-              />
-            </div>
-            <div className="changePassword">
-              <div
-                className={
-                  switchPassword === true ? "passwordBox" : "noDisplay"
-                }
-              >
+              <hr />
+              <div className="mailBox">
+                Email adress
+                <input
+                  type="email"
+                  className="inputBottomProfil"
+                  placeholder="Enter Your Email"
+                />
+              </div>
+              <div className="changePassword">
                 <div className="oldPassword">
                   Current Password
                   <input
@@ -262,23 +291,124 @@ const Profil = () => {
                     placeholder="New password"
                   />
                 </div>
+                <div />
               </div>
-              <button
-                id={
-                  switchPassword === false ? "passwordButton" : "validateButton"
-                }
-                type="button"
-                onClick={() => setSwitchpassWord(!switchPassword)}
-              >
-                {switchPassword === false
-                  ? "Modify your password"
-                  : "Validate new password"}
-              </button>
-              <div />
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="rightBoxMain">
+          <div className="mainTitleProfil">
+            <img src={iconProfil} />
+            <h1>PROFILE</h1>
+          </div>
+
+          <div className="bigBoxRight">
+            <div className="titleProfil">PUBLIC INFORMATIONS</div>
+            <div className="topBoxProfil">
+              <div className="boxDateAndBoxPhoto">
+                <div className="boxPhoto">
+                  <label htmlFor="buttonPicture">
+                    {user.profil_picture !== null ? (
+                      <img
+                        src={imageUrl}
+                        alt="userPicture"
+                        name="myFile"
+                        className="userPicture"
+                        id="profilPictureForm"
+                      />
+                    ) : (
+                      <img
+                        className="userPicture"
+                        src={Add2}
+                        alt="logo of a cross"
+                      />
+                    )}
+                  </label>
+                  <input
+                    type="file"
+                    id="buttonPicture"
+                    accept="image/*"
+                    onChange={(e) => {
+                      handlePictureChange(e)
+                    }}
+                    style={{ display: "none" }}
+                  />
+                </div>
+                <span>Register since {shortDate}</span>
+              </div>
+              <div className="boxName"></div>
+              <div className="localisationBox">
+                <span>Country</span>
+                <input
+                  type="text"
+                  className="inputCountryCity"
+                  placeholder="Enter your country"
+                />
+                <span>City</span>
+                <input
+                  type="text"
+                  className="inputCountryCity"
+                  placeholder="Enter Your City"
+                />
+              </div>
+            </div>
+            <div className="bioBoxProfil">
+              <div className="bioBoxProfilText">
+                <div className="bioBoxProfilTitle">Bio on Profil</div>
+              </div>
+            </div>
+            <div className="gameBoxProfil">
+              <div className="gameBoxProfilTitle">
+                <h2>SEARCH TO PLAY ON</h2>
+              </div>
+              <div className="hrDiv">
+                <hr />
+              </div>
+
+              <div className="gameBoxGamesList">composant gamelist</div>
+            </div>
+            <div className="hrDiv">
+              <hr />
+            </div>
+            <div className="bottomBoxProfil">
+              <div className="privateInfoBox">
+                <h3>PERSONAL</h3>
+                <h3>INFORMATIONS</h3>
+              </div>
+
+              <hr />
+              <div className="mailBox">
+                Email adress
+                <input
+                  type="email"
+                  className="inputBottomProfil"
+                  placeholder="Enter Your Email"
+                />
+              </div>
+              <div className="changePassword">
+                <div className="oldPassword">
+                  Current Password
+                  <input
+                    type="password"
+                    className="inputBottomProfil"
+                    placeholder="Current password"
+                  />
+                </div>
+                <div className="newPassword">
+                  New Password
+                  <input
+                    type="password"
+                    className="inputBottomProfil"
+                    placeholder="New password"
+                  />
+                </div>
+                <div />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
