ed64a8bc486cfc17dbcdc70d5a729d1565ad4f1d
Dagets <inard.frederic@gmail.com>
2023-09-12 16:37:53 +0200

    ok

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `guilden` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `guilden`;
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for macos13 (arm64)
 --
 -- Host: 127.0.0.1    Database: guilden
 -- ------------------------------------------------------
