1abc735fec4bc85e1461659d1daa1527b45948dd
Dagets <inard.frederic@gmail.com>
2023-09-12 19:55:57 +0200

    MAJ topic

--- frontend/src/pages/Topics.jsx
+++ frontend/src/pages/Topics.jsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 import NewTopic from "../components/NewTopic/NewTopic"
+import Cookies from "js-cookie"
 
 export default function Topics() {
+  const tokenFromCookie = Cookies.get("authToken")
   const [topics, setTopics] = useState([])
   const [isNewTopicOpen, setIsNewTopicOpen] = useState(false) // État pour gérer l'ouverture de la fenêtre modale
+  const headers = {
+    Authorization: `Bearer ${tokenFromCookie}`
+  }
 
   useEffect(() => {
-    axios.get("http://localhost:4242/topics").then((res) => setTopics(res.data))
+    axios
+      .get("http://localhost:4242/topics", { headers })
+      .then((res) => setTopics(res.data))
   }, [])
+  console.info("ICI", topics)
 
   const openNewTopicModal = () => {
     setIsNewTopicOpen(true)
