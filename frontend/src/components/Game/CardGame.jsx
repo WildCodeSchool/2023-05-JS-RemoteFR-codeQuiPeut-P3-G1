4e44ab9f189bbbfe56aa6f766c7b2289be6681cc
Dagets <inard.frederic@gmail.com>
2023-09-13 18:51:17 +0200

    ok

--- frontend/src/components/Game/CardGame.jsx
+++ frontend/src/components/Game/CardGame.jsx
@@ -9,3 +9,27 @@ export default function CardGame({ onClose }) {
     </div>
   )
 }
+
+// SELECT
+//     u.username AS username,
+//     u.profil_picture AS profil_picture,
+//     g.schedule AS schedule,
+//     g.description AS description,
+//     g.type AS type,
+//     g.guild_name AS guild_name,
+//     g.city AS city,
+//     g.is_remote AS is_remote,
+//     g.is_campaign AS is_campaign,
+//     g.gm_username AS gm_username
+// FROM
+//     games AS g
+// INNER JOIN
+//     games_has_users AS gu
+// ON
+//     g.id = gu.games_id
+// INNER JOIN
+//     users AS u
+// ON
+//     gu.users_id = u.id
+// WHERE
+//     g.id = VOTRE_ID_DE_PARTIE;
