8c7cb10eb74b8b6b91a5fef8987ad691fede08b4
HpJkR <m.hertereau@gmail.com>
2023-09-20 10:46:28 +0200

    create popup notification

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,8 +3,10 @@ import axios from "axios"
 import Cookies from "js-cookie"
 import AuthContext from "../components/AuthContext/AuthContext"
 
-import questionMark from "../assets/icon-dashboard/questionMark.png"
-import Notepad from "../assets/icon-dashboard/Notepad.png"
+import questionMark from "../assets/icon-dashboard/questionMark.svg"
+import Notifications from "../assets/icon-dashboard/icon-Notifications.svg"
+import Notepad from "../assets/icon-dashboard/Notepad.svg"
+import CrossWithBG from "../assets/icon-dashboard/crossWithBG.svg"
 
 import MyProfil from "../components/Dashboard/MyProfil"
 import FuturGames from "../components/Dashboard/FuturGames"
@@ -58,6 +60,12 @@ const Dashboard = () => {
         })
   }, [])
 
+  const [isPopupOpen, setPopupOpen] = useState(false)
+
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen)
+  }
+
   return (
     <>
       <div className="mainDivDashboard">
@@ -66,13 +74,93 @@ const Dashboard = () => {
             <h1>Dashboard</h1>
           </div>
           <div className="iconRight">
-            <img id="logoNotepad" src={Notepad} alt="logo of notepad" />
+            <img
+              id="logoNotepad"
+              src={Notepad}
+              alt="logo of notepad"
+              onClick={togglePopup}
+            />
             <img
               id="logoQuestionMark"
               src={questionMark}
               alt="logo of question mark"
             />
           </div>
+          {isPopupOpen && (
+            <div className="popupNotification">
+              <div className="contentPopUpNotification">
+                <div className="buttonClosePopUpNotification">
+                  <img
+                    src={CrossWithBG}
+                    alt="icon close popup of notification"
+                    onClick={togglePopup}
+                  />
+                </div>
+                {/* <div className="Notifications-content">
+                  <div className="Invitations">
+                    {joiningRequestData.map((request) => (
+                      <div className="rawInvitations" key={request.id}>
+                        <div className="invitProfilPicture">
+                          <img
+                            src={`${import.meta.env.VITE_BACKEND_URL}/${
+                              request.profil_picture
+                            }`}
+                            alt="profil picture"
+                          />
+                        </div>
+                        <div className="invitedName">
+                          <p>{request.username}</p>
+                        </div>
+                        <div className="wantsToJoin">
+                          <span>has invited to join:</span>
+                        </div>
+                        <div className="requesterGame">
+                          <p>{request.guild_name}</p>
+                        </div>
+                        <div className="requesterButton">
+                          <img
+                            id="validateButton"
+                            src={check}
+                            alt="validate"
+                            onClick={() =>
+                              acceptedRequest(
+                                request.requester_id,
+                                request.games_id
+                              )
+                            }
+                          />
+                          <img
+                            id="refuseButton"
+                            src={crossDash}
+                            alt="refuse"
+                            onClick={() =>
+                              openConfirmation(
+                                request.requester_id,
+                                request.games_id
+                              )
+                            }
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  {showConfirm && (
+                    <div className="custom-confirmRequest">
+                      <div className="modalCustomConfirmRequest">
+                        <p>Are you sure you want to reject this request ?</p>
+                        <div className="buttonCustomConfirmRequest">
+                          <button id="buttonNo" onClick={closeConfirmation}>
+                            NO
+                          </button>
+                          <button onClick={confirmReject}>YES</button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div> */}
+              </div>
+            </div>
+          )}
         </div>
         <div className="dashboardAllComponents">
           <div className="friends-games-container">
